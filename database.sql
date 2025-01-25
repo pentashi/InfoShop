07da15180a5c0b07d634629851e0401c5a9437de
Nifras <nifrasusanar@outlook.com>
2025-01-25 17:39:13 +0530

    Missing quotation and quotation items

--- database.sql
+++ database.sql
@@ -1,3 +1,40 @@
+CREATE TABLE IF NOT EXISTS `quotations` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `contact_id` bigint unsigned NOT NULL,
+  `quotation_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quotation_date` date NOT NULL,
+  `expiry_date` date DEFAULT NULL,
+  `quotation_terms` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `subject` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `subtotal` decimal(10,2) NOT NULL,
+  `discount` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `total` decimal(10,2) NOT NULL,
+  `profit` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `customer_notes` text COLLATE utf8mb4_unicode_ci,
+  `terms_conditions` text COLLATE utf8mb4_unicode_ci,
+  `custom_fields` json DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `quotations_quotation_number_unique` (`quotation_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `quotation_items` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `quotation_id` bigint unsigned NOT NULL,
+  `product_id` bigint unsigned DEFAULT NULL,
+  `custom_description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` decimal(10,2) NOT NULL,
+  `cost` decimal(10,2) DEFAULT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 ALTER TABLE quotations ADD COLUMN store_id BIGINT UNSIGNED NULL AFTER id;
 ALTER TABLE quotation_items ADD COLUMN batch_id BIGINT UNSIGNED NULL AFTER id;
 ALTER TABLE quotation_items ADD COLUMN discount DECIMAL(8, 2) NULL AFTER price;
